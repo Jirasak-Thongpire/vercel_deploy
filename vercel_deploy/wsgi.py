251f70b0ffa7d1ab312be56525f122f97a4ed150
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-07-14 20:00:59 +0700

    fix wsgi

--- vercel_deploy/wsgi.py
+++ vercel_deploy/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_deploy.settings')
 
 application = get_wsgi_application()
-
 app = application  # For Vercel compatibility
