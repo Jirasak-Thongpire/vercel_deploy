c571e75cdef0243de4c74d5e2abc2bd73ac78103
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-07-14 20:24:57 +0700

    fix config

--- vercel_deploy/wsgi.py
+++ vercel_deploy/wsgi.py
@@ -1,13 +1,18 @@
+"""
+WSGI config for deploy_demo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
 import os
-import sys
+
 from django.core.wsgi import get_wsgi_application
 
-# Add the project directory to the path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_deploy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploy_demo.settings')
 
 application = get_wsgi_application()
 
-# For Vercel compatibility
 app = application
